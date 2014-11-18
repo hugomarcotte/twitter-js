8e94c1e6257f1ab44a250107f75d05859d27b73c
Hugo Marcotte <marcotte.hugo@gmail.com>
2014-11-18 14:59:12 -0500

    All selects work

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,12 @@ var Tweet = require('../models').Tweet;
 /* GET home page. */
 router.get('/', function(req, res) {
 
-	// GET ONE USER AND FIND ALL TWEETS FOR THAT USER
-	// User.find(1).complete(function(err,user) {
-	// 		user.getTweets().complete(function(err,tweets) {
-	// 				console.log("our test:"+tweets);
-	// 	})
-	// });
-
 	// Get all tweets
-	Tweet.findAll().complete(function(err, tweets){
+
+	// Find how to get users and display name
+	Tweet.findAll( {include: [ User ] }).complete(function(err, tweets){
+
+console.log(tweets[0].User.name);
 
 		res.render('index', {
 			title: 'Twitter.js Awesome stuff',
@@ -24,7 +21,6 @@ router.get('/', function(req, res) {
 			show_form: true
 		});
 
-		console.log("tweet: "+tweets.length);
 	});
 
 
@@ -32,23 +28,52 @@ router.get('/', function(req, res) {
 
 
 router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  var list = store.find({name: name});
 
-  res.render('index', {
-  	title: 'Twitter.js - Posts by '+name,
-  	tweets: list,
-  	show_form: true,
-  	username: name
-  });
+	var name = req.params.name;
+
+	// Get user by name
+	User.find({ where : { name: name } })
+	.complete(function(err, user){
+
+		// Get all tweets for user
+		Tweet.findAll({ where: { UserId: user.id }, include: [ User ] })
+		.complete(function(err, tweets){
+
+			// On success render view passing user name and tweets
+			res.render('index', {
+				title: 'Twitter.js - Posts by '+ user.name,
+				tweets: tweets,
+				show_form: true,
+				username: user.name
+			});
+
+		});
+		// .catch(function(err){
+		// 	console.log("Error getting tweets for user: " + err);
+		// });
+
+	})
+	// .error(function(err){
+	// 	console.log("Error getting user by name: " + err);
+	// });
+
 });
 
-router.get('/users/:name/tweets/:id', function(req, res) {
-	var name = req.params.name;
-	var tweetId = Number(req.params.id);
-	var list = store.find({id: tweetId});
+router.get('/tweets/:id', function(req, res) {
 
-	res.render('index', { title: 'Twitters.js - A single tweet by '+name, tweets: list })
+	// find tweet by ID
+	var tweetId = Number(req.params.id);
+
+	Tweet.findAll({ where: { id: tweetId }, include: [ User ] })
+	.complete(function(err, tweets){
+		res.render('index', { title: 'Twitters.js - A single tweet by ', tweets: tweets })
+	});
+	// .catch(function(err){
+	// 	console.log("Error getting tweet by ID: " + err);
+	// });
+
+
+
 })
 
 module.exports = router;
